9648bd77186f8077f732d9f6ff3000c68d16392f
Raafat Elmenayar <raafat.el123@gmail.com>
2024-02-20 16:38:16 +0200

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bukki/core/errors/failure.dart';
+import 'package:bukki/features/home/data/models/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+}
