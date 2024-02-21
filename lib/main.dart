f2e3850275ecbe798683e6bcb456ee8bec85669e
Raafat Elmenayar <raafat.el123@gmail.com>
2024-02-21 17:55:00 +0200

    setup Service Locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:bukki/core/constants/constants.dart';
 import 'package:bukki/core/router/app_router.dart';
+import 'package:bukki/core/utils/service_locator.dart';
+import 'package:bukki/features/home/data/repos/home_repo_impl.dart';
+import 'package:bukki/features/home/presentation/manager/best_seller/best_seller_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'features/home/presentation/manager/books_banner/books_banner_cubit.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const Bukki());
 }
 
@@ -12,13 +18,21 @@ class Bukki extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: kPrimaryColor,
-          textTheme:
-              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => BooksBannerCubit(getIt.get<HomeRepoImpl>())),
+        BlocProvider(
+            create: (context) => BestSellerBooksCubit(getIt.get<HomeRepoImpl>())),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimaryColor,
+            textTheme:
+                GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
+      ),
     );
   }
 }
