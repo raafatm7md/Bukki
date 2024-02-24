844cf71116ad52c1f0231f14734e407d5e4c0b35
Raafat Elmenayar <raafat.el123@gmail.com>
2024-02-25 00:26:50 +0200

    home repo implementation

---
+++ Clean Architecture/lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,43 @@
+import 'package:bukki/core/errors/failure.dart';
+import 'package:bukki/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bukki/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bukki/features/home/domain/entities/book_entity.dart';
+import 'package:bukki/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+  HomeRepoImpl(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchBestNewestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchBestNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books);
+      }
+      books = await homeRemoteDataSource.fetchBestNewestBooks();
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books);
+      }
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
